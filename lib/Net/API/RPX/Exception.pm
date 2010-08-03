3d20177af912e5c5b398dfe40ebdb31c34ae946b
Kent Fredric <kentfredric@gmail.com>
2010-08-03 23:33:44 +1200

    Wow, hax, but works!

--- lib/Net/API/RPX/Exception.pm
+++ lib/Net/API/RPX/Exception.pm
@@ -4,10 +4,35 @@ package Net::API::RPX::Exception;
 use strict;
 use warnings;
 
-use Moose;
-extends 'Exception::Class::Base';
+=head1 NAME
 
-__PACKAGE__->meta->make_immutable;
+Net::API::RPX::Exception - A hackish baseclass fusing L<Moose> with L<Exception::Class>
+
+=cut
+
+
+use Moose;
+extends qw( Exception::Class::Base Moose::Object );
+
+around new => sub {
+  my ( $orig, $class, @args ) = @_;
+  my @mooseargs;
+looper: {
+    for my $i ( 0 .. $#args / 2 ) {
+      my ($key) = $args[ $i * 2 ];
+      if ( $class->meta->has_attribute($key) ) {
+        push @mooseargs, splice @args, $i * 2, 2, ();
+        redo looper;
+      }
+    }
+  }
+  my $obj = $class->$orig(@args);
+  return $class->meta->new_object(
+    __INSTANCE__ => $obj,
+    @mooseargs,
+  );
+};
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
 
 1;
 
--- lib/Net/API/RPX/Exception/Usage.pm
+++ lib/Net/API/RPX/Exception/Usage.pm
@@ -3,6 +3,13 @@ package Net::API::RPX::Exception::Usage;
 # $Id:$
 use strict;
 use warnings;
+
+=head1 NAME
+
+Net::API::RPX::Exception::Usage - For when the interface is used wrongly.
+
+=cut
+
 use Moose;
 extends 'Net::API::RPX::Exception';
 
@@ -18,14 +25,23 @@ sub _signature_string {
 
 sub _explanation {
   my ($self) = @_;
-  return sprintf q{Method %s on package %s expects the parameter "%s"}.qq{\n}.qq{\t%s\n}  , $self->method_name , $self->package , $self->required_parameter, $self->_signature_String;
+  return sprintf q{Method %s on package %s expects the parameter "%s"} . qq{\n} . qq{\t%s\n}, $self->method_name, $self->package,
+    $self->required_parameter, $self->_signature_string;
 }
 
+=head1 METHODS
+
+=head2 full_message
+
+Overrides Exception::Class::Base 's stringification that is applied to the top of the backtrace.
+
+=cut
+
 sub full_message {
-    my $self = shift;
-    my $msg = $self->message . qq{\n} . $self->_explanation;
-    return $msg;
+  my $self = shift;
+  my $msg  = $self->message . qq{\n} . $self->_explanation;
+  return $msg;
 }
-__PACKAGE__->meta->make_immutable;
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 1;
 
